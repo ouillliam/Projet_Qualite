4afe73acfd97245fd23dda196ce3e88be455fed1
Benjamin GUERIOT <90306512+BenjaminGueriot@users.noreply.github.com>
2023-03-03 12:31:29 +0100

    maj pom + DiscreteActionTest

---
+++ src/test/java/fr/usmb/action_test/DiscreteActionTest.java
@@ -0,0 +1,67 @@
+package fr.usmb.action_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import timer.PeriodicTimer;
+import timer.Timer;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import action.DiscreteAction;
+
+class DiscreteActionTest {
+	private DiscreteAction discrete;
+	private DiscreteAction discrete2;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+		Object object = new String("test");
+		Timer timer = new PeriodicTimer(2);
+		Timer timer2 = new PeriodicTimer(2);
+		
+		discrete = new DiscreteAction(object, "toUpperCase", timer);
+		discrete2 = new DiscreteAction(object, "toUpperCase", timer2);
+		
+		assertTrue(discrete.hasNext());
+		discrete.next();
+		assertTrue(discrete.hasNext());
+		
+		discrete2.next();
+		
+		assertEquals(2,discrete2.getCurrentLapsTime());
+		
+		assertEquals(0,discrete.compareTo(discrete2));
+		
+		discrete.spendTime(1);
+		
+		assertTrue(discrete.hasNext());
+		
+		assertEquals(discrete.getObject(),object);
+		
+		assertEquals(-1,discrete.compareTo(discrete2));
+		
+		//fail("Not yet implemented");
+	}
+
+}
